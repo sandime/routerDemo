e03fec5ddf4862f3f38730daf8360b55603a2156
sherri <sherri@sherricruz.com>
2015-03-14 16:29:28 -0700

    angular router demo

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SHERRI on 3/14/15.
+ */
+var express = require('express');
+var app = express();
+var path = require('path');
+//set the client folder to serve public assets
+
+app.use(express.static(__dirname + '/public'));
+
+//set up one route to the index.html file
+
+app.get('*', function (req, resp) {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+
+});
+//start the server on port 8080
+app.listen(8080);
+console.log('hey, magic here on port 8080.');

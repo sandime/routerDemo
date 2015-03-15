d04bd68fb74dab01514da6d97425f01469ccd7a6
sherri <sherri@sherricruz.com>
2015-03-14 21:08:42 -0700

    all works. $locationProvider.html5Mode(true); was in the wrong place. ugh

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -2,31 +2,34 @@
  * Created by SHERRI on 3/14/15.
  */
 // inject ngRoute for all our routing needs
-var app = angular.module("routerApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+var app = angular.module("routerRoutes", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider) {
 // route for the home page
-    $routeProvider.when('/', {
-        templateUrl: 'public/views/pages/home.html',
+    $routeProvider
+        .when('/', {
+        templateUrl: 'views/pages/home.html',
         controller: 'homeController',
         controllerAs: 'home'
     })
 // route for the about page
         .when('/about', {
-            templateUrl: 'public/views/pages/about.html',
+            templateUrl: 'views/pages/about.html',
             controller: 'aboutController',
             controllerAs: 'about'
         })
 // route for the contact page
         .when('/contact', {
-            templateUrl: 'public/views/pages/contact.html',
+            templateUrl: 'views/pages/contact.html',
             controller: 'contactController',
             controllerAs: 'contact'
         })
      .otherwise({
     redirectTo: "/"
   });
+    //set our app up to have pretty URLS
+    $locationProvider.html5Mode(true);
 });
-// set our app up to have pretty URLS
-//$locationProvider.html5Mode(true);
+
 
 
